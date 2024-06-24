7579844571a3e683265f3873c89b5cdab9c625a7
Padliwinata <rpadliwinata@gmail.com>
2024-06-24 10:25:15 +0700

    update

--- auth.py
+++ auth.py
@@ -1,7 +1,8 @@
 import typing
 
 from db import db_user
-from utils import encrypt_password, create_access_token
+from models import User
+from utils import encrypt_password, decrypt_password, create_access_token
 
 
 def register(username: str, raw_password: str) -> typing.Optional[bool, dict]:
@@ -12,11 +13,15 @@ def register(username: str, raw_password: str) -> typing.Optional[bool, dict]:
     return db_user.put({'username': username, 'password': encrypt_password(raw_password)})
 
 
-def login(username: str, password: str):
+def login(username: str, password: str) -> typing.Optional[bool, str]:
     fetch_response = db_user.fetch({'username': username})
     if fetch_response.count == 0:
         return False
 
-    return create_access_token({'username': username})
+    user = User(**fetch_response.items[0])
+    if decrypt_password(user.password) == password:
+        return create_access_token({'username': username})
+
+    return False
 
 
