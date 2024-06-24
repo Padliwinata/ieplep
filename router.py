d92e35be118e1124f4c5cfa6bbf72db02255c78b
Padliwinata <rpadliwinata@gmail.com>
2024-06-24 10:26:48 +0700

    fixed

--- router.py
+++ router.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 
-pg = st.navigation([st.Page('main.py', title='Main'), st.Page('presensi.py', title='Presensi Generator')])
+pg = st.navigation([st.Page('app/main.py', title='Main'), st.Page('app/presensi.py', title='Presensi Generator')])
 pg.run()
