e78b41e5db0bf54c5d44b3e3776e1b215bc15480
Padliwinata <rpadliwinata@gmail.com>
2024-06-25 10:36:09 +0700

    update

--- router.py
+++ router.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 
 # if 'user' not in st.session_state:
-pg = st.navigation([st.Page('app/main.py', title='Soal Semester Pendek')])
+pg = st.navigation([st.Page('app/main.py', title='Soal Semester Pendek'),
+                    st.Page('app/enroll.py', title='Enroll Mahasiswa')])
 # else:
 # pg = st.navigation([st.Page('app/presensi.py', title='Presensi Generator')])
 pg.run()
