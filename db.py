66091b17125f08519c5563b018b89368f2223288
Padliwinata <rpadliwinata@gmail.com>
2024-09-11 11:40:46 +0700

    update

--- db.py
+++
@@ -1,7 +0,0 @@
-import deta
-import streamlit as st
-
-
-deta_obj = deta.Deta(st.secrets['DATA_KEY'])
-db_user = deta_obj.Base("mahasiswa")
-
--- router.py
+++ router.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 
 # if 'user' not in st.session_state:
-# pg = st.navigation([st.Page('app/main.py', title='Soal Semester Pendek'),
-#                     st.Page('app/enroll.py', title='Enroll Mahasiswa')])
+pg = st.navigation([st.Page('app/presensi.py', title='Soal Semester Pendek')])
 # else:
-pg = st.navigation([st.Page('app/presensi.py', title='Presensi Generator')])
+# pg = st.navigation([st.Page('app/presensi.py', title='Presensi Generator')])
 pg.run()
