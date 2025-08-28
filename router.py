cbd739937b0dcec4c674d8c4b464657c9a6091ea
Anandito Satria Asyraf <ananditosatriaa181@gmail.com>
2025-08-28 16:42:13 +0700

    [ADJ] Delete some unused file

--- router.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-# if 'user' not in st.session_state:
-pg = st.navigation([st.Page('app/presensi.py', title='Soal Semester Pendek')])
-# else:
-# pg = st.navigation([st.Page('app/presensi.py', title='Presensi Generator')])
-pg.run()
